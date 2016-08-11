40ada3b7acb6af28d88480e67d4314adbbeb4892
yomboprime <yomboprime@users.noreply.github.com>
2016-08-11 02:32:26 +0200

    Added break demo to file list.

--- files.js
+++ files.js
@@ -3,6 +3,7 @@ var files = {
 	    "webgl_gpgpu_erosion",
 		"webgl_gpgpu_water",
         "webgl_gpgpu_protoplanet",
-        "webgl_gpgpu_birds"
+        "webgl_gpgpu_birds",
+        "webgl_physics_convex_break"
 	]
 };
